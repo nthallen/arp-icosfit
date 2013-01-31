7f32ac466590168a16a5b22f41c74aa1c12658b6
Norton Allen <allen@huarp.harvard.edu>
2013-01-31 16:44:35 +0000

    Modified to use new modular ICOS_setup

--- Matlab/ICOSfit/get_nu.m
+++ Matlab/ICOSfit/get_nu.m
@@ -1,4 +1,4 @@
-function nu = get_nu( varargin );
+function nu = get_nu( varargin )
 % nu_rel = get_nu(scannums, x [, PTEfile]);
 %  Returns the relative wavenumber determined from the
 %  etalon fit. Absolute wavenumber for a given sample
@@ -13,28 +13,29 @@ if ischar(varargin{1})
   region = varargin{1};
   suffix = varargin{2};
   x = varargin{3};
-  if length(varargin) >= 4
+  base = [ 'ICOSout.' region ];
+  if ~isempty(suffix)
+    base = [ base '.' suffix ];
+  end
+  if exist( base, 'dir' )
+    S = ICOS_setup(base); % To define nu0, nu_F0
+    if ~isfield(S,'nu0') || ~isfield(S,'nu_F0')
+      error('ICOSsetup did not define nu0 or nu_F0');
+    end
+    if any(diff(S.scannum)==0) || (any(diff(S.scannum)<0) && any(diff(S.scannum)>0))
+      error('ScanNum is non-monotonic in %s', base );
+    end
+  else
+    error('Cannot locate directory %s', base );
+  end
+  PTEfile = S.PTEfile;
+  if length(varargin) >= 4 && ~isempty(varargin{4})
     PTEfile = varargin{4};
   end
   if length(varargin) >= 5
     scannums = varargin{5};
   else
-    scannums = fitline('region', region);
-  end
-  base = [ 'ICOSout.' region ];
-  if length(suffix)
-    base = [ base '.' suffix ];
-  end
-  if exist( base, 'dir' )
-    eval('ICOSsetup'); % To define nu0, nu_F0
-    if ~exist('nu0','var') || ~exist('nu_F0', 'var')
-      error('ICOSsetup did not define nu0 or nu_F0');
-    end
-    if any(diff(scannum)==0) || (any(diff(scannum)<0) && any(diff(scannum)>0))
-      error('ScanNum is non-monotonic in %s', base );
-    end
-  else
-    error('Cannot locate directory %s', base );
+    scannums = S.scannum; % fitline('region', region);
   end
 else
   scannums = varargin{1};
@@ -43,7 +44,7 @@ else
     PTEfile = varargin{3};
   end
 end
-if length(PTEfile) == 0
+if isempty(PTEfile)
   PTEfile = 'PTE.txt';
 end
 PTE = load(PTEfile);
@@ -53,18 +54,18 @@ FSR = cellparams.fsr; % 0.0198;
 for i=1:length(scannums)
   j = find(PTE(:,1)==scannums(i));
   if length(j) > 1
-    error(sprintf('PTE file has duplicate entries for scannums %d', scannums(i)));
-  elseif length(j) == 0
-    error(sprintf('Cannot locate PTE row for scannums %d', scannums(i)));
+    error('PTE file has duplicate entries for scannums %d', scannums(i));
+  elseif isempty(j)
+    error('Cannot locate PTE row for scannums %d', scannums(i));
   end
-  Y = PTE(j,[5:11]);
+  Y = PTE(j,5:11);
   xx = (x-PTE(j,4)+1)*1e-3;
   nu(:,i) = -FSR*etln_evalJ(Y,xx);
 %   if size(PTE,2) == 12
 %     nu(:,i) = nu(:,i) + PTE(j,12);
 %   end
 end
-if length(region)
-  fdi = interp1( scannum, 1:length(scannum), scannums );
-  nu = nu + nu0 + ones(length(x),1)*nu_F0(fdi)';
+if ~isempty(region)
+  fdi = interp1( S.scannum, 1:length(S.scannum), scannums );
+  nu = nu + S.nu0 + ones(length(x),1)*S.nu_F0(fdi)';
 end
