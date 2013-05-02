c29805f2fbe789c7e535279227ad8854ddc743a7
Norton Allen <allen@huarp.harvard.edu>
2013-05-02 19:48:31 +0000

    Add optional scaling matrix. Default is appropriate for the current wiring of HWV as of MACPEX through 5/2013 (at least)

--- Matlab/ICOSfit/rewrite_scans.m
+++ Matlab/ICOSfit/rewrite_scans.m
@@ -1,20 +1,24 @@
-function rewrite_scans(ibase,obase)
+function rewrite_scans(ibase,obase,M)
 %This program is useful if spectra are inverted. It is better to fix it in hardware. 
 % Should be customized for each axis and probably a specific version should be made 
 % and copied into local directories.
-if nargin <1
+if nargin < 1 || isempty(ibase)
    ibase = 'SSP';
 end
-if nargin <2
+if nargin < 2 || isempty(obase)
    obase = 'SSPo';
 end
+if nargin < 2 || isempty(M)
+    M = [ 1 0 0; 0 1 0; 0 0 -1];
+end
+    
 index = 1;
 while 1
   pi = mlf_path(ibase,index);
   fi = loadbin(pi);
-  if isempty(fi) break; end
+  if isempty(fi); break; end
   po = mlf_path(obase,index);
-  fo = fi * [ 1 0 0; 0 1 0; 0 0 -1];
+  fo = fi * M;
   temp=fo(:,1);
   fo(:,1)=fo(:,3);
   fo(:,3)=temp;
