ea61cb6d7e20c6bcb6be3513b566c2551a29618d
Norton Allen <allen@huarp.harvard.edu>
2013-05-28 14:54:43 +0000

    Upgraded to use ICOS_setup

--- Matlab/ICOSfit/laser_drift.m
+++ Matlab/ICOSfit/laser_drift.m
@@ -1,21 +1,17 @@
-function [drift,scannum] = laser_drift(base, ptefile);
+function [drift,scannum] = laser_drift(base, ptefile)
 % [drift,scannum] = laser_drift(base, ptefile);
-if nargin < 2 || length(ptefile) == 0
+if nargin < 2 || isempty(ptefile)
   ptefile = 'PTE.txt';
 end
 P = load(ptefile);
 ca = P(:,1);
-fn = P(:,5)*0.0198;
-oldpwd = pwd;
-cd(base);
-ICOSconfig;
-fd = load('ICOSsum.dat');
-cd(oldpwd);
-cb = fd(:,6);
-nu_F0 = fd(:,n_base_params+n_input_params+1);
-fnb = interp1(ca,fn,cb);
-drift = fnb - nu_F0;
+cellparams = load_cell_cfg;
+EtalonFSR = cellparams.fsr;
+fn = P(:,5) * EtalonFSR;
+S = ICOS_setup(base);
+fnb = interp1(ca,fn,S.scannum);
+drift = fnb - S.nu_F0;
 drift = drift-drift(1);
 if nargout > 1
-  scannum = cb;
+  scannum = S.scannum;
 end
