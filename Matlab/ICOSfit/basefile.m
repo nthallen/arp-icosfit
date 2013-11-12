25bf6cbbf8ecfbfd478c2549d65bc8f4d8d86c89
David Sayres <sayres@huarp.harvard.edu>
2013-11-12 16:54:17 +0000

    now can use fitline region or scan numbers as input

--- Matlab/ICOSfit/basefile.m
+++ Matlab/ICOSfit/basefile.m
@@ -1,6 +1,11 @@
 function [Unew,V,S]=basefile(region,nsvds,suffix,range)
 %function [U,V,S]=basefile(region,nsvds,suffix)
-[data]=loadscans([],region);
+if isnumeric(region)
+  scannum = region;
+else
+  scannum = fitline('region',region);
+end
+[data]=loadscans([],scannum);
 if nargin<4
     range=300:1831;
 end
@@ -10,8 +15,8 @@ figure; for i=1:size(U,2); nsubplot(size(U,2),1,i,1); plot(U(:,i)); end
 addzoom
 Unew = zeros(length(data),size(U,2));
 Unew(range,:)=U;
-writebase('sbase.dat',Unew,S,V);
+writeskewbase(region,Unew,S,V);
 if nargin>=3
-    eval(sprintf('!mv sbase.dat sbase%i.%s.dat',region(1),suffix))
-    disp(sprintf('Moved sbase file to sbase%i.%s.dat\n',region(1),suffix))
+    eval(sprintf('!mv sbase.dat sbase%i.%s.dat',scannum(1),suffix))
+    disp(sprintf('Moved sbase file to sbase%i.%s.dat\n',scannum(1),suffix))
 end
