0fe9d2b5da2abb87caa06744a8cb9782df08a36a
Norton Allen <allen@huarp.harvard.edu>
2015-02-09 20:58:05 +0000

    More sophisticated analysis, including phase and confidence intervals

--- Matlab/ICOSfit/feedback_analysis.m
+++ Matlab/ICOSfit/feedback_analysis.m
@@ -1,5 +1,6 @@
-function  [osc_out, beta_out, S_out] = feedback_analysis(varargin)
-% feedback_analysis([scans,] base [, base ...]);
+function  [osc_out, beta_out, S_out, sigosc_out, sigbeta_out] = feedback_analysis(varargin)
+% [osc_out, beta_out, S_out, sigosc_out, sigbeta_out] = ...
+%    feedback_analysis([scans,] base [, base ...]);
 % 
 % feedback_analysis takes one to three arguments:
 %   An optional list of scan numbers to use in the analysis
@@ -7,10 +8,11 @@ function  [osc_out, beta_out, S_out] = feedback_analysis(varargin)
 %   The base name of an icosfit run to fully analyze.
 scans = [];
 bi = 1;
-if isnumeric(varargin{1})
+if ~isempty(varargin) && isnumeric(varargin{1})
   scans = varargin{1};
   bi = 2;
 end
+if length(varargin) >= bi && strnicmp(varargin{bi},
 if bi > length(varargin)
   help feedback_analysis
   return;
@@ -23,25 +25,35 @@ if nargout > 2
 end
 osc = zeros(length(bases),size(S0.Chi,2));
 beta = osc;
-[osc(1,:), beta(1,:)] = fit_comp([],S0, line_pos0, scans);
+sigosc = osc;
+sigbeta = osc;
+[osc(1,:), beta(1,:), sigosc(1,:), sigbeta(1,:)] = fit_comp([],S0, line_pos0, scans);
 for i = 2:length(bases)
   [line_pos,S] = get_line_position(bases{i});
-  [ osc(i,:), beta(i,:) ] = fit_comp(S0, S, line_pos, scans);
+  [ osc(i,:), beta(i,:), sigosc(i,:), sigbeta(i,:) ] = fit_comp(S0, S, line_pos, scans);
   if nargout > 2
     S_out{i} = S;
+    if nargout > 3
+      sigosc_out = sigosc;
+      if nargout > 4
+        sigbeta_out = sigbeta;
+      end
+    end
   end
 end
 
 f = figure;
-lnums = 1:length(osc);
-plot(lnums,osc','s-');
+lnums = 1:size(osc,2);
+% plot(lnums,osc','s-');
+errorbar(lnums, osc', 2*sigosc');
 legend(bases);
 set(gca,'YGrid','On','XDir','Reverse','xlim',[0.75 lnums(end)+0.25]);
 title(sprintf('%s: Residual Oscillation by Line',getrun));
 set(f,'Name',sprintf('%s Summary',getrun));
 
 f = figure;
-plot(lnums, beta', 's-');
+% plot(lnums, beta', 's-');
+errorbar(lnums, beta', 2*sigbeta');
 legend(bases);
 set(gca,'YGrid','On','XDir','Reverse','xlim',[0.75 lnums(end)+0.25]);
 title(sprintf('%s: Phase of oscilation',getrun));
@@ -53,30 +65,59 @@ set(f,'Name',sprintf('%s Phase',getrun));
 % ylabel('Phase');
 % title(sprintf('%s: Phase plot',getrun));
 % set(f,'Name',sprintf('%s: Phase plot',getrun));
-f = figure;
-plot(S0.nu, beta');
-title(sprintf('%s: Phase vs Wavenumber', getrun));
-xlabel('cm^{-1}');
-ylabel('Phase deg');
-set(f, 'Name', sprintf('%s: Phase vs cm-1', getrun));
+
+% f = figure;
+% plot(S0.nu, beta');
+% title(sprintf('%s: Phase vs Wavenumber', getrun));
+% xlabel('cm^{-1}');
+% ylabel('Phase deg');
+% set(f, 'Name', sprintf('%s: Phase vs cm-1', getrun));
 
 f = figure;
-polar(deg2rad(beta'), osc','.-');
+% polar(deg2rad(beta'), osc','.-');
+maxosc = max(max(osc+2*sigosc));
+polar(0, maxosc, 'w');
+hold on;
+colors = 'bgkr';
+for i=1:size(osc,1)
+  bc = colors(mod(i-1,length(colors))+1);
+  for j=1:size(osc,2)
+    % draw individual point
+    polar(deg2rad(beta(i,j)), osc(i,j), ['.' bc]);
+    % Label point with line number
+    xc = cos(deg2rad(beta(i,j))) * osc(i,j);
+    yc = sin(deg2rad(beta(i,j))) * osc(i,j);
+    text(xc,yc,sprintf(' %d',j));
+    % Draw uncertainty box
+    th = beta(i,j) + 2*[-1:.1:1 1:-.1:-1 -1]*sigbeta(i,j);
+    rho = osc(i,j) + 2*[-1*ones(1,21) ones(1,21) -1]*sigosc(i,j);
+    polar(deg2rad(th), rho, bc);
+  end
+end
+hold off;
 title(sprintf('%s: Phase Plot',getrun));
 set(f,'Name',sprintf('%s: Phase Plot',getrun));
+drawnow;
 
 if nargout > 0
   osc_out = osc;
   if nargout > 1
     beta_out = beta;
+    if nargout > 3
+      sigosc_out = sigosc;
+      if nargout > 4
+        sigbeta_out = sigbeta;
+      end
+    end
   end
 end
 
 
-function [osc_out, beta_out] = fit_comp(S0, S, line_pos, scans)
+function [osc_out, beta_out, sigosc_out, sigbeta_out] = fit_comp(S0, S, line_pos, scans)
 if isempty(scans)
   scans = S.scannum;
 end
+use_stat = 1;
 [scans,vs] = scan_intersect(scans, S.scannum);
 PTE = load(S.PTEfile);
 [Pscans,vp] = scan_intersect(scans, PTE(:,1));
@@ -106,6 +147,8 @@ fn = a*(X.^2) + b*X + c*row + (d*row).*exp(-(1./tau)*X) + ...
 
 osc = zeros(1,size(S.Chi,2));
 beta_out = osc;
+sigosc_out = osc;
+sigbeta_out= osc;
 for loi=1:size(S.Chi,2)
   f = figure;
   set(f,'Name',sprintf('%s: Line %d',S.base,loi));
@@ -168,16 +211,33 @@ for loi=1:size(S.Chi,2)
  
   % This is a fit to an arbitrary sine wave
   SM = [1+0*ffn3, sin(2*pi*ffn3), cos(2*pi*ffn3)];
-  sa = SM\Chires10;
-  osc(loi) = sqrt(sa(2)^2 + sa(3)^2);
+
+  if use_stat
+    [sa,saint,r,rint,stats] = regress(Chires10,SM,1-0.6827);
+    sigsa = saint(:,2)-sa;
+    sigsa2 = sqrt((sa(2)^2)*(sigsa(2)^2) + (sa(3)^2)*(sigsa(3)^2));
+    osc(loi) = sqrt(sa(2)^2 + sa(3)^2);
+    sigosc_out(loi) = sigsa2/osc(loi);
+    sigbeta_out(loi) = (sigsa2/(osc(loi)^2)) * 180/pi;
+  else
+    sa = SM\Chires10;
+    osc(loi) = sqrt(sa(2)^2 + sa(3)^2);
+  end
+  beta = 90 - (atan2(sa(3),sa(2)) * 180 / pi);
+  if beta < 0
+    beta = beta + 360;
+  end
+  beta_out(loi) = beta;
+
   xffn = (0:.01:1)';
   vffnfit = sa(1) + sa(2)*sin(2*pi*xffn) + sa(3)*cos(2*pi*xffn);
-  beta = 90 - (atan2(sa(3),sa(2)) * 180 / pi);
-  beta_out(loi) = beta;
   
   ax = nsubplot(2,3,[2 2],3,xsp);
   %scatter(ffn3,Chires10,[],scan10);
   plot(ax,ffn3*360,Chires10,'.',xffn*360,vffnfit,'r');
+  % hold on;
+  % errorbarxy(ax, beta, osc(loi)+sa(1), sigbeta_out(loi), sigosc_out(loi));
+  xlim([0 360]);
   title(ax,sprintf('%s: Line %d, \\beta = %.0f^o', getrun, loi, beta));
   xlabel(ax,'Etalon Fractional fringe position at line center');
   ylabel(ax,'Mixing Ratio RDFM');
