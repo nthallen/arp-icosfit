57e7ea0639fdc2c6a27363985af3c84b5b7e61a4
Norton Allen <allen@huarp.harvard.edu>
2015-03-05 16:59:39 +0000

    More options

--- Matlab/ICOSfit/feedback_analysis.m
+++ Matlab/ICOSfit/feedback_analysis.m
@@ -8,11 +8,15 @@ function  [osc_out, beta_out, S_out, sigosc_out, sigbeta_out] = feedback_analysi
 %   The base name of an icosfit run to fully analyze.
 scans = [];
 bi = 1;
+noplot = 0;
 if ~isempty(varargin) && isnumeric(varargin{1})
   scans = varargin{1};
   bi = 2;
 end
-%if length(varargin) >= bi && strnicmp(varargin{bi})
+if length(varargin) >= bi && strcmpi(varargin{bi},'noplot')
+  bi = bi + 1;
+  noplot = 1;
+end
 if bi > length(varargin)
   help feedback_analysis
   return;
@@ -27,10 +31,12 @@ osc = zeros(length(bases),size(S0.Chi,2));
 beta = osc;
 sigosc = osc;
 sigbeta = osc;
-[osc(1,:), beta(1,:), sigosc(1,:), sigbeta(1,:)] = fit_comp([],S0, line_pos0, scans);
+[osc(1,:), beta(1,:), sigosc(1,:), sigbeta(1,:)] = ...
+  fit_comp([],S0, line_pos0, scans, noplot);
 for i = 2:length(bases)
   [line_pos,S] = get_line_position(bases{i});
-  [ osc(i,:), beta(i,:), sigosc(i,:), sigbeta(i,:) ] = fit_comp(S0, S, line_pos, scans);
+  [ osc(i,:), beta(i,:), sigosc(i,:), sigbeta(i,:) ] = ...
+    fit_comp(S0, S, line_pos, scans, noplot);
   if nargout > 2
     S_out{i} = S;
     if nargout > 3
@@ -42,36 +48,38 @@ for i = 2:length(bases)
   end
 end
 
-f = figure;
-lnums = 1:size(osc,2);
-% plot(lnums,osc','s-');
-errorbar(lnums, osc', 2*sigosc');
-legend(bases);
-set(gca,'YGrid','On','XDir','Reverse','xlim',[0.75 lnums(end)+0.25]);
-title(sprintf('%s: Residual Oscillation by Line',getrun));
-set(f,'Name',sprintf('%s Summary',getrun));
-
-f = figure;
-% plot(lnums, beta', 's-');
-errorbar(lnums, beta', 2*sigbeta');
-legend(bases);
-set(gca,'YGrid','On','XDir','Reverse','xlim',[0.75 lnums(end)+0.25]);
-title(sprintf('%s: Phase of oscilation',getrun));
-set(f,'Name',sprintf('%s Phase',getrun));
-
-% f = figure;
-% plot(osc', beta','.-');
-% xlabel('Amplitude');
-% ylabel('Phase');
-% title(sprintf('%s: Phase plot',getrun));
-% set(f,'Name',sprintf('%s: Phase plot',getrun));
-
-% f = figure;
-% plot(S0.nu, beta');
-% title(sprintf('%s: Phase vs Wavenumber', getrun));
-% xlabel('cm^{-1}');
-% ylabel('Phase deg');
-% set(f, 'Name', sprintf('%s: Phase vs cm-1', getrun));
+if noplot == 0
+  f = figure;
+  lnums = 1:size(osc,2);
+  % plot(lnums,osc','s-');
+  errorbar(lnums, osc', 2*sigosc');
+  legend(bases);
+  set(gca,'YGrid','On','XDir','Reverse','xlim',[0.75 lnums(end)+0.25]);
+  title(sprintf('%s: Residual Oscillation by Line',getrun));
+  set(f,'Name',sprintf('%s Summary',getrun));
+  
+  f = figure;
+  % plot(lnums, beta', 's-');
+  errorbar(lnums, beta', 2*sigbeta');
+  legend(bases);
+  set(gca,'YGrid','On','XDir','Reverse','xlim',[0.75 lnums(end)+0.25]);
+  title(sprintf('%s: Phase of oscilation',getrun));
+  set(f,'Name',sprintf('%s Phase',getrun));
+  
+  % f = figure;
+  % plot(osc', beta','.-');
+  % xlabel('Amplitude');
+  % ylabel('Phase');
+  % title(sprintf('%s: Phase plot',getrun));
+  % set(f,'Name',sprintf('%s: Phase plot',getrun));
+  
+  % f = figure;
+  % plot(S0.nu, beta');
+  % title(sprintf('%s: Phase vs Wavenumber', getrun));
+  % xlabel('cm^{-1}');
+  % ylabel('Phase deg');
+  % set(f, 'Name', sprintf('%s: Phase vs cm-1', getrun));
+end
 
 f = figure;
 % polar(deg2rad(beta'), osc','.-');
@@ -95,7 +103,7 @@ for i=1:size(osc,1)
   end
 end
 hold off;
-title(sprintf('%s: Phase Plot',getrun));
+title(sprintf('%s: %d-%d: Phase Plot',getrun, min(scans), max(scans)));
 set(f,'Name',sprintf('%s: Phase Plot',getrun));
 drawnow;
 
@@ -113,9 +121,14 @@ if nargout > 0
 end
 
 
-function [osc_out, beta_out, sigosc_out, sigbeta_out] = fit_comp(S0, S, line_pos, scans)
-if isempty(scans)
+%--------------------------------------------------------
+function [osc_out, beta_out, sigosc_out, sigbeta_out] = ...
+  fit_comp(S0, S, line_pos, scans, noplot)
+if nargin < 4 || isempty(scans)
   scans = S.scannum;
+end
+if nargin < 5
+  noplot = 0;
 end
 use_stat = 1;
 [scans,vs] = scan_intersect(scans, S.scannum);
@@ -150,31 +163,33 @@ beta_out = osc;
 sigosc_out = osc;
 sigbeta_out= osc;
 for loi=1:size(S.Chi,2)
-  f = figure;
-  set(f,'Name',sprintf('%s: Line %d',S.base,loi));
-  p = get(f,'Position');
-  new_h = 500;
-  new_w = 1500;
-  p = [ p(1)-(new_w-p(3))/2 p(2)-(new_h-p(4)) new_w new_h];
-  set(f,'Position',p)
-  xsp = .08;
-  if isempty(S0)
-    ax = nsubplot(1,3,1,1,xsp);
-    plot(ax(1),scan10, fastavg(S.Chi(vs,loi),10),'.');
-    title(ax(1), sprintf('%s: Line %d', getrun, loi));
-    ylabel(ax(1),S.base);
-    xlabel(ax(1),'Scan Number');
-  else
-    ax = [ nsubplot(2,3,1,1,xsp) nsubplot(2,3,2,1,xsp) ];
-    plot(ax(1),scan010, fastavg(S0.Chi(vs0,loi),10),'.');
-    title(ax(1), sprintf('%s: Line %d', getrun, loi));
-    ylabel(ax(1),sprintf('Ref: %s',S0.base));
-    set(ax(1),'XTickLabel',[]);
-    plot(ax(2),scan10, fastavg(S.Chi(vs,loi),10),'.');
-    ylabel(ax(2),sprintf('%s',S.base));
-    xlabel(ax(2),'Scan Number');
-    set(ax(2),'YAxisLocation','Right');
-    linkaxes(ax,'x');
+  if noplot == 0
+    f = figure;
+    set(f,'Name',sprintf('%s: Line %d',S.base,loi));
+    p = get(f,'Position');
+    new_h = 500;
+    new_w = 1500;
+    p = [ p(1)-(new_w-p(3))/2 p(2)-(new_h-p(4)) new_w new_h];
+    set(f,'Position',p)
+    xsp = .08;
+    if isempty(S0)
+      ax = nsubplot(1,3,1,1,xsp);
+      plot(ax(1),scan10, fastavg(S.Chi(vs,loi),10),'.');
+      title(ax(1), sprintf('%s: Line %d', getrun, loi));
+      ylabel(ax(1),S.base);
+      xlabel(ax(1),'Scan Number');
+    else
+      ax = [ nsubplot(2,3,1,1,xsp) nsubplot(2,3,2,1,xsp) ];
+      plot(ax(1),scan010, fastavg(S0.Chi(vs0,loi),10),'.');
+      title(ax(1), sprintf('%s: Line %d', getrun, loi));
+      ylabel(ax(1),sprintf('Ref: %s',S0.base));
+      set(ax(1),'XTickLabel',[]);
+      plot(ax(2),scan10, fastavg(S.Chi(vs,loi),10),'.');
+      ylabel(ax(2),sprintf('%s',S.base));
+      xlabel(ax(2),'Scan Number');
+      set(ax(2),'YAxisLocation','Right');
+      linkaxes(ax,'x');
+    end
   end
 
   Chimean = mean(S.Chi(vs,loi));
@@ -189,25 +204,27 @@ for loi=1:size(S.Chi,2)
   ffn3 = fn310 - floor(fn310);
   Chires10 = fastavg(Chires,Navg);
 
-  ax = [ nsubplot(2,3,1,2,xsp) nsubplot(2,3,2,2,xsp) ];
-  plot(ax(1),scan10,Chires10,'.');
-  title(ax(1),sprintf('%s: Line %d', getrun, loi));
-  set(ax(1),'XTickLabel',[]);
-  ylabel(ax(1),'Mixing Ratio RDFM');
-  plot(ax(2),scan10,ffn3,'.');
-  set(ax(2),'YAxisLocation','Right');
-  xlabel(ax(2),'Scan Number');
-  ylabel(ax(2),'Fractional Fringe');
-  set(ax(2),'YAxisLocation','Right');
-  linkaxes(ax,'x');
+  if noplot == 0
+    ax = [ nsubplot(2,3,1,2,xsp) nsubplot(2,3,2,2,xsp) ];
+    plot(ax(1),scan10,Chires10,'.');
+    title(ax(1),sprintf('%s: Line %d', getrun, loi));
+    set(ax(1),'XTickLabel',[]);
+    ylabel(ax(1),'Mixing Ratio RDFM');
+    plot(ax(2),scan10,ffn3,'.');
+    set(ax(2),'YAxisLocation','Right');
+    xlabel(ax(2),'Scan Number');
+    ylabel(ax(2),'Fractional Fringe');
+    set(ax(2),'YAxisLocation','Right');
+    linkaxes(ax,'x');
+  end
   
   % This is a fit to a V
-%   xffn = [0, 0.5, 1];
-%   vxffn = 0.5*[1, -1, 1];
-%   vffn = interp1(xffn,vxffn,ffn3);
-%   Vffn = polyfit(vffn,Chires10,1);
-%   vffnfit = polyval(Vffn,vxffn);
-%   osc(loi) = Vffn(1);
+  % xffn = [0, 0.5, 1];
+  % vxffn = 0.5*[1, -1, 1];
+  % vffn = interp1(xffn,vxffn,ffn3);
+  % Vffn = polyfit(vffn,Chires10,1);
+  % vffnfit = polyval(Vffn,vxffn);
+  % osc(loi) = Vffn(1);
  
   % This is a fit to an arbitrary sine wave
   SM = [1+0*ffn3, sin(2*pi*ffn3), cos(2*pi*ffn3)];
@@ -232,16 +249,18 @@ for loi=1:size(S.Chi,2)
   xffn = (0:.01:1)';
   vffnfit = sa(1) + sa(2)*sin(2*pi*xffn) + sa(3)*cos(2*pi*xffn);
   
-  ax = nsubplot(2,3,[2 2],3,xsp);
-  %scatter(ffn3,Chires10,[],scan10);
-  plot(ax,ffn3*360,Chires10,'.',xffn*360,vffnfit,'r');
-  % hold on;
-  % errorbarxy(ax, beta, osc(loi)+sa(1), sigbeta_out(loi), sigosc_out(loi));
-  xlim([0 360]);
-  title(ax,sprintf('%s: Line %d, \\beta = %.0f^o', getrun, loi, beta));
-  xlabel(ax,'Etalon Fractional fringe position at line center');
-  ylabel(ax,'Mixing Ratio RDFM');
-  drawnow;
+  if noplot == 0
+    ax = nsubplot(2,3,[2 2],3,xsp);
+    %scatter(ffn3,Chires10,[],scan10);
+    plot(ax,ffn3*360,Chires10,'.',xffn*360,vffnfit,'r');
+    % hold on;
+    % errorbarxy(ax, beta, osc(loi)+sa(1), sigbeta_out(loi), sigosc_out(loi));
+    xlim([0 360]);
+    title(ax,sprintf('%s: Line %d, \\beta = %.0f^o', getrun, loi, beta));
+    xlabel(ax,'Etalon Fractional fringe position at line center');
+    ylabel(ax,'Mixing Ratio RDFM');
+    drawnow;
+  end
 end
 
 osc_out = osc;
