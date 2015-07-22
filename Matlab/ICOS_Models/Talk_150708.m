61e50bf2138d9d29605b20f790eeabf923c0d2fd
Norton Allen <allen@huarp.harvard.edu>
2015-07-22 14:52:11 +0000

    Talk documentation

--- Matlab/ICOS_Models/Talk_150708.m
+++ Matlab/ICOS_Models/Talk_150708.m
@@ -1,17 +1,17 @@
-%%
+%% ICOS_Model5
 load('IB5_150707_100x100.mat');
 %%
 IB.Animate(15);
-%%
+%% ICOS_Model6 3cm
 load('IB6_150707_100x100_r3.mat');
 %%
 IB.Animate(15);
-%%
+%% ICOS_Model6 2cm
 load('IB6_150707_100x100.mat');
 %%
 IB.Animate(15);
-%%
+%% ICOS_Model6a 2cm
 load('IB6a_150707_100x100.mat');
 %%
 IB.Animate(15);
-%%
+
