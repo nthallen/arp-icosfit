1bbfd40f063db06b28f61f2611f50fa0dd43b917
Norton Allen <allen@huarp.harvard.edu>
2016-03-04 15:12:59 +0000

    Add acceptance angle to detector

--- Matlab/RayTrace/detector.m
+++ Matlab/RayTrace/detector.m
@@ -4,9 +4,12 @@ classdef detector < optic
   end
   
   methods
-    function det = detector(l, O, D, vis)
+    function det = detector(l, O, D, vis, theta)
+      if nargin < 5
+        theta = 0;
+      end
       det = det@optic('det', O, D, 1, 1, vis);
-      det.Surface{1} = square(l, O, D, 0, 0, 1, 1);
+      det.Surface{1} = square(l, O, D, 0, 0, 1, 1, theta);
       det.Surface{2} = square(l, O-0.1*D, -D, 0, 0, 1, 1);
     end
   end
