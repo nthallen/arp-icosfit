d8df1b607f1cade0a43652469bb543130eb0184c
Norton Allen <allen@huarp.harvard.edu>
2016-05-05 16:20:08 +0000

    Add 3rd axis for color

--- Matlab/ICOS_Models/rt_quick_plot.m
+++ Matlab/ICOS_Models/rt_quick_plot.m
@@ -1,4 +1,9 @@
-function rt_quick_plot(res, fld1, fld2)
+function rt_quick_plot(res, fld1, fld2, fld3)
+  % rt_quick_plot(res, fld1, fld2);
+  % rt_quick_plot(res, fld1, fld2, fld3);
+  % Examples:
+  %   rt_quick_plot(res, 'R1', 'L');
+  %   rt_quick_plot(res, 'R1', 'R2', 'L');
   figure;
   Xdata = [res.(fld1)];
   if nargin == 2
@@ -9,4 +14,12 @@ function rt_quick_plot(res, fld1, fld2)
     plot(Xdata,Ydata,'.');
     xlabel(strrep(fld1,'_','\_'));
     ylabel(strrep(fld2,'_','\_'));
+  elseif nargin == 4
+    Ydata = [res.(fld2)];
+    Cdata = [res.(fld3)];
+    scatter(Xdata,Ydata,[],Cdata);
+    xlabel(strrep(fld1,'_','\_'));
+    ylabel(strrep(fld2,'_','\_'));
+    cb = colorbar;
+    cb.Label.String = fld3;
   end
