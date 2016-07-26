21b91d2bbe6f84d5949f3ff4709d45b1582fbc40
David Sayres <sayres@huarp.harvard.edu>
2016-07-26 19:36:28 +0000

    added option for multiple regions.

--- Matlab/ICOSfit/basefilefromresid.m
+++ Matlab/ICOSfit/basefilefromresid.m
@@ -1,13 +1,14 @@
 function [Unew,V,S]=basefilefromresid(region,suffix,nsvds,name)
 %function [U,V,S]=basefile(region,nsvds,suffix)
-base = [ 'ICOSout.' region ];
-if ~isempty(suffix); base = [ base '.' suffix ]; end
-lines=[];
-by_molecule=0;
-ICOSsetup;
 xmin = [];
 resids = [];
 nscans = 0;
+for j=1:length(region)
+    base = [ 'ICOSout.' region{j} ];
+    if ~isempty(suffix); base = [ base '.' suffix ]; end
+    lines=[];
+    by_molecule=0;
+    ICOSsetup;
 for i = scannum'
   path = mlf_path( base, i );
   f = load(path);
@@ -28,6 +29,7 @@ for i = scannum'
     resids(f(:,1)-xmin+1,nscans) = f(:,3)-f(:,4)+f(:,5);
   end
 end
+end
 x = xmin:xmin+size(resids,1)-1;
 [I,J]=find(resids==0);
 resids(unique(I),:)=NaN;
@@ -39,8 +41,8 @@ figure; for i=1:size(U,2); nsubplot(size(U,2),1,i,1); plot(U(:,i)); end
 addzoom
 Unew = zeros(max(x),size(U,2));
 Unew(x,:)=U;
-writeskewbase(region,Unew,S,V);
+writeskewbase(region{1},Unew,S,V);
 if nargin>=3
-    eval(sprintf('!mv sbase.dat sbase%i.%s.dat',scannum(1),name))
+    eval(sprintf('!mv sbase.dat sbase%i.%s.dat',scannum(j),name))
     disp(sprintf('Moved sbase file to sbase%i.%s.dat\n',scannum(1),name))
 end
