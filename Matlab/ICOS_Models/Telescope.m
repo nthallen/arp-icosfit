435a1d70dd9fb970f8b45db6b5fb9b71ac198486
Norton Allen <allen@huarp.harvard.edu>
2016-08-14 20:13:08 -0400

    Telescope Model

---
+++ Matlab/ICOS_Models/Telescope.m
@@ -0,0 +1,162 @@
+classdef Telescope < opt_model_p
+  % Optical model of a telescope for investigating effects of divergence
+  % Optic:
+  %  1: Lens: ZC-PM-12-50
+  %  2: Lens: ZC-PM-12-12
+  %  3: Detector
+  % The salient features include:
+  %   Distance between elements
+  
+  
+  properties
+    % apparently all the important properties are maintained in the
+    % opt_model_p and opt_model structures.
+  end
+  
+  methods
+    function PM = Telescope(P,varargin)
+      PM = PM@opt_model_p(P,varargin{:});
+    end
+
+   
+    function Res = evaluate_endpoints(PM,P,n_pts)
+      Res = PM.results_struct();
+      if P.evaluate_endpoints < 0
+        return;
+      elseif P.evaluate_endpoints > 0
+        opt_n = P.evaluate_endpoints;
+      elseif P.plot_endpoints > 0
+        opt_n = P.plot_endpoints;
+      else
+        opt_n = length(PM.M.Optic);
+      end
+      Res = PM.results_struct;
+      Res.inside = PM.M.Inside;
+      Res.max_rays = PM.M.max_rays;
+      [xyz, oxyz] = PM.M.extract_endpoints(opt_n);
+      if nargin < 3
+        n_pts = size(xyz,1);
+      end
+      
+      % Total power calculation
+      vf = find([PM.M.Rays(1:PM.M.n_rays).n_opt] == opt_n);
+      Res.total_power = 0;
+      for i = 1:length(vf)
+        Res.total_power = Res.total_power + PM.M.Rays(i).ray.P;
+      end
+      
+      % Max radius
+      Res.max_radius = max(sqrt(sum(xyz(:,[2 3]).^2,2)));
+      
+      % Max divergence
+      dxyz = xyz-oxyz;
+      dxyz = diag(1./sqrt(sum(dxyz.^2,2)))*dxyz;
+      ryz = diag(1./sqrt(sum(xyz(:,[2,3]).^2,2)))*(xyz*diag([0,1,1]));
+      diverg = sum(dxyz.*ryz,2);
+      Res.max_divergence = asind(nanmax(diverg));
+    end
+ 
+  end
+  
+  methods (Static)
+    function P = props
+      P.Lenses = { 'ZC_PM_12_50', 'ZC_PM_12_12' };
+      P.L1_Space = 1; % Space before L1
+      P.L2_Space = 6.35; % Space before L2
+      P.optics_n = 2.4361;
+      P.D_l = 0.2;
+      P.D_Space = 1; % Space before Detector
+      P.D_dY = 0;
+      P.D_dZ = 0;
+      P.beam_diameter = 0.4;
+      P.beam_divergence = 0;
+      P.beam_n_th = 12;
+      P.beam_n_r = 5;
+%       P.Beam_y = 0;
+%       P.Beam_z = P.beam_diameter/2;
+%       P.Beam_dy = 0;
+%       P.Beam_dz = 0;
+      isp_meniscuses;
+      
+      P.max_rays = 60;
+      P.visible = false;
+      P.edges_visible = true;
+      P.visibility = [];
+      P.view = [];
+      P.plot_endpoints = 0;
+      P.evaluate_endpoints = 0;
+      P.propagate = 1;
+    end
+    
+    function M = P_model(P)
+      n_optics = length(P.Lenses)+1;
+      visibility = P.visible * ones(n_optics,1);
+      if length(P.visibility) <= n_optics
+        visibility(1:length(P.visibility)) = P.visibility;
+      end
+      lenses_n = P.optics_n;
+      air_n = 1;
+      P.max_rays = P.beam_n_th*P.beam_n_r*n_optics;
+      M = opt_model(n_optics, P.max_rays);
+      M.Spot_Size = P.beam_diameter;
+      M.visible = P.visible;
+      opt_X = 0;
+      opt_n = 1;
+      Lens_Space = [P.L2_Space,P.D_Space];
+      for i = 1:length(P.Lenses)
+        L = P.LensTypes.(P.Lenses{i});
+        if strcmp(L.type,'positive_meniscus')
+          if L.EFL > 0
+            D = [-1,0,0];
+          else
+            D = [1, 0, 0];
+          end
+          M.Optic{opt_n} = positive_meniscus(L.r, L.R1, L.R2, L.CT, L.EFL, ...
+            sprintf('L%d', i), [opt_X,0,0], D, lenses_n, air_n, ...
+            P.visible && visibility(opt_n));
+        elseif strcmp(L.type,'negative_meniscus')
+          M.Optic{opt_n} = negative_meniscus(L.r, L.R1, L.R2, L.CT, L.EFL, ...
+            sprintf('L%d', i), [opt_X,0,0], [-1,0,0], lenses_n, air_n, ...
+            P.visible && visibility(opt_n));
+        end
+        M.Optic{opt_n}.edges_visible = P.edges_visible;
+        opt_X = opt_X + L.CT;
+        opt_X = opt_X + Lens_Space(i);
+        opt_n = opt_n + 1;
+      end
+      M.Optic{opt_n} = detector(P.D_l, [opt_X,P.D_dY,P.D_dZ], [-1,0,0], ...
+        P.visible && visibility(opt_n), 15);
+      divl = sind(P.beam_divergence);
+      if P.beam_n_r < 1
+        rs = P.beam_diameter/2;
+      else
+        rs = linspace(0,P.beam_diameter/2,P.beam_n_r);
+      end
+      if P.propagate
+        for r = rs
+          for th = [0:P.beam_n_th-1]*pi*2/P.beam_n_th
+            Oincident = [-P.L1_Space,r*cos(th),r*sin(th)];
+            Dincident = [1,0,0] + (2/P.beam_diameter)*divl*Oincident;
+            Eincident = Oincident + P.L1_Space*Dincident;
+            M.push_ray(opt_ray(Oincident, Dincident), 0, 0, 1);
+            M.propagate;
+            if r == 0
+              break;
+            end
+          end
+        end
+      end
+    end
+    
+    function Res = results_struct
+      Res.total_power = [];
+      Res.max_radius = [];
+      Res.max_divergence = [];
+      Res.inside = [];
+      Res.overlap = [];
+      Res.mean_angle = [];
+      Res.n_rays = [];
+      Res.max_rays = [];
+    end
+  end
+end
---
+++ Matlab/ICOS_Models/scratch_160814.m
@@ -0,0 +1,113 @@
+%%
+% Telescope diagnostics
+%
+% This first section starts with zero divergence and just demonstrates
+% that as originally specified, the output beam is somewhat convergent.
+P = Telescope.props;
+P.visible = 0;
+P.evaluate_endpoints = 3;
+% P.L2_Space = 6.088;
+PM = Telescope(P,'beam_diameter',[0.1 0.2 0.3 0.4 0.5]);
+PM.plot_results('max_divergence');
+%%
+% Just visualize the last optic
+P.visible = 1;
+P.beam_diameter = 0.1;
+P.visibility = [0 0];
+PM = Telescope(P);
+%%
+% This shows that for parallel inputs, divergence stays pretty small
+% at different L2_Space values. Zero is near 6.096 (for 0.4 beam at 1 cm)
+P.visible = 0;
+P.evaluate_endpoints = 3;
+P.beam_n_r = 1;
+PM = Telescope(P,'L2_Space',[6.085:.001:6.15]);
+PM.plot_results('max_divergence');
+grid;
+%%
+% Now let's look at how this changes when divergence is added
+% Output divergence increases at 4*input divergence.
+P = Telescope.props;
+P.visible = 0;
+P.evaluate_endpoints = 3;
+P.L2_Space = 6.096;
+PM = Telescope(P,'beam_divergence',[0:.02:0.3]);
+PM.plot_results('max_divergence');
+hold on;
+plot(PM.Results.beam_divergence,PM.Results.beam_divergence*4);
+grid;
+legend('output divergence','input divergence * 4','location','southeast');
+% Confirms that divergence scales up by 4
+%%
+PM.plot_results('max_radius');
+% this shows beam radius is actually 1/4 and improves slighly with divergence
+% That makes sense because in the input divergence pushes the focal point
+% out, making the radius smaller at L2, but that means the angular
+% correction is less.
+%%
+% Now let's see what happens if we try to improve the focus:
+% It appears we should be able to reduce the divergence by
+% pushing L2 out to 6.8 (from ~6.1) without significantly increasing
+% the output beam diameter
+P = Telescope.props;
+P.visible = 0;
+P.evaluate_endpoints = 3;
+P.L2_Space = 6.096;
+P.beam_diameter = 0.4;
+P.beam_divergence = 0.3; % degrees
+P.beam_n_r = 1;
+dL2S = linspace(0,1,50);
+PM = Telescope(P,'L2_Space', P.L2_Space + dL2S);
+figure;
+PM.plot_results('max_divergence');
+grid;
+figure;
+PM.plot_results('max_radius');
+grid;
+%%
+close all
+%%
+% This is just a test to verify that we aren't diverging
+% Just push the detector out and verify the max_radius does not increase
+% appreciably.
+P = Telescope.props;
+P.visible = 0;
+P.evaluate_endpoints = 3;
+P.beam_diameter = 0.4;
+P.beam_divergence = 0.3; % degrees
+P.L2_Space = 6.8; % Matched to 0.3
+PM = Telescope(P,'D_Space',1:20);
+PM.plot_results('max_radius');
+%%
+PM.plot_results('max_divergence');
+%%
+% Now let's try to model the HCl system as currently configured, with
+% 18 cm between the laser and L1, L2_Space of 6.35 cm and ~20 cm before M1
+P = Telescope.props;
+P.visible = 1;
+P.evaluate_endpoints = 3;
+P.beam_diameter = 0.4;
+P.beam_divergence = 0.3; % degrees
+P.L1_Space = 18;
+% P.L2_Space = 6.35;
+P.D_Space = 20;
+PM = Telescope(P);
+view(0,0);
+%%
+% Now let's find an optimal L2_Space
+P.beam_n_r = 1;
+P.visible = 0;
+dL2S = linspace(0,1,50);
+PM = Telescope(P,'L2_Space', P.L2_Space + dL2S);
+figure;
+PM.plot_results('max_divergence');
+grid;
+figure;
+PM.plot_results('max_radius');
+grid;
+%%
+% I get 6.78: Now let's see how this looks:
+P.L2_Space = 6.78;
+P.visible = 1;
+PM = Telescope(P);
+view(0,0);
